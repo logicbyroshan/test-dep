bab2c6ec3db0daee9aab0586f0809982892c1a82
Roshan Damor <iamroshandamor@gmail.com>
2025-08-08 14:23:02 +0530

    changed somthin

--- config/settings.py
+++ config/settings.py
@@ -6,9 +6,8 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
